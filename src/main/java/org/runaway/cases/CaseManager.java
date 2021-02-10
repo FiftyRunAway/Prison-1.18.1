edb813be61c7485e0ba14b30e823d3452d606b31
sasha <sir.runaway@yandex.ru>
2021-02-10 14:21:58 +0300

    case

--- src/main/java/org/runaway/cases/CaseManager.java
+++ src/main/java/org/runaway/cases/CaseManager.java
@@ -20,9 +20,28 @@ public class CaseManager {
                 .techName("defaultKey")
                 .rollTimes(5)
                 .stopAfter(15)
-                .rewardItems(Arrays.asList(LootItem.builder().prisonItem(ItemManager.getPrisonItem("star")).amount(2).probability(5).build(),
-                        LootItem.builder().prisonItem(ItemManager.getPrisonItem("steak")).probability(15).build(),
-                        MoneyReward.builder().amount(2).probability(10).build()))
+                .rewardItems(Arrays.asList(
+                        LootItem.builder().prisonItem(ItemManager.getPrisonItem("star")).minAmount(1).maxAmount(2).probability(6).build(),
+                        LootItem.builder().prisonItem(ItemManager.getPrisonItem("steak")).probability(50).build(),
+                        LootItem.builder().prisonItem(ItemManager.getPrisonItem("apple")).minAmount(2).maxAmount(8).probability(100).build(),
+                        LootItem.builder().prisonItem(ItemManager.getPrisonItem("arrow")).minAmount(2).maxAmount(6).probability(25).build(),
+                        LootItem.builder().prisonItem(ItemManager.getPrisonItem("gapple")).amount(2).probability(6).build(),
+                        LootItem.builder().prisonItem(ItemManager.getPrisonItem("podvalPass")).probability(3).build(),
+                        LootItem.builder().prisonItem(ItemManager.getPrisonItem("wspade0_1")).probability(13).build(),
+                        LootItem.builder().prisonItem(ItemManager.getPrisonItem("wspade1_2")).probability(7).build(),
+                        LootItem.builder().prisonItem(ItemManager.getPrisonItem("wspade2_3")).probability(5).build(),
+                        LootItem.builder().prisonItem(ItemManager.getPrisonItem("wpickaxe0_1")).probability(13).build(),
+                        LootItem.builder().prisonItem(ItemManager.getPrisonItem("wpickaxe1_2")).probability(7).build(),
+                        LootItem.builder().prisonItem(ItemManager.getPrisonItem("wpickaxe2_3")).probability(5).build(),
+                        LootItem.builder().prisonItem(ItemManager.getPrisonItem("bow0_1")).probability(5).build(),
+                        LootItem.builder().prisonItem(ItemManager.getPrisonItem("swpickaxe0_1")).probability(2).build(),
+                        LootItem.builder().prisonItem(ItemManager.getPrisonItem("wsword0_1")).probability(10).build(),
+                        LootItem.builder().prisonItem(ItemManager.getPrisonItem("lhelmet0_1")).probability(5).build(),
+                        LootItem.builder().prisonItem(ItemManager.getPrisonItem("lchestplate0_1")).probability(7).build(),
+                        LootItem.builder().prisonItem(ItemManager.getPrisonItem("lleggings0_1")).probability(7).build(),
+                        LootItem.builder().prisonItem(ItemManager.getPrisonItem("lboots0_1")).probability(7).build(),
+                        LootItem.builder().prisonItem(ItemManager.getPrisonItem("fishing0_1")).probability(6).build(),
+                        MoneyReward.builder().amount(2).probability(35).build()))
                 .build().init();
     }
     public static CaseRefactored getCase(String techName) {
